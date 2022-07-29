6f9b09bf138e831a5743d1e0882f6179b408413a
lakwet <axellepiot@hotmail.com>
2022-07-29 15:20:50 +0200

    Add chvatal simplex algo

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+/// Fundamental theorem of linear programming:
+/// Any linear program, given in standard form, either
+/// 1. has an optimal solution with a finite objective value.
+/// 2. is infeasible or
+/// 3. is unbounded
+/// (Introduction to algorithm, 2009, Cormen, Leiserson, et al)
+
+pub mod algo;
 #[allow(non_snake_case)]
 mod forms;
 pub mod shared;
